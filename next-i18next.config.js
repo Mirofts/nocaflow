8bb7a334a57472eb3bdcabede015d97c4d588d20
Mirofts <mirofts@gmail.com>
2025-07-01 13:39:41 +0000

    Spécifier la version de Node.js pour Vercel build

---
+++ next-i18next.config.js
@@ -0,0 +1,9 @@
+// next-i18next.config.js
+/** @type {import('next-i18next').UserConfig} */
+const i18nConfig = {
+  i18n: {
+    defaultLocale: 'fr',
+    locales: ['en', 'fr'],
+  },
+};
+module.exports = i18nConfig;
