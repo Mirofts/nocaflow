ccb51fcdcd71a5283a2f75718776f8e4050ca2e7
Mirofts <mirofts@gmail.com>
2025-07-02 10:33:26 +0000

    Feat: Add AuthContext and ThemeProvider to _app.js, crucial for app initialization

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,20 @@
 import '@/styles/globals.css';
-import { appWithTranslation } from 'next-i18next'; // <-- Importez ceci
-
-// REMARQUE : NE PAS IMPORTER '../lib/i18n'; ici si vous l'avez supprimé ou vidé.
+import { appWithTranslation } from 'next-i18next';
+import { AuthContextProvider } from '../context/AuthContext'; // <-- Importez AuthContextProvider
+import { ThemeProvider } from '../context/ThemeContext';     // <-- Importez ThemeProvider
 
 function MyApp({ Component, pageProps }) {
   console.log("MyApp simple test rendering"); // Debug log
   return (
-    <main>
-      <Component {...pageProps} />
-    </main>
+    // Enveloppez l'application avec vos Context Providers
+    <AuthContextProvider> {/* Fournit le contexte d'authentification à toute l'app */}
+      <ThemeProvider> {/* Fournit le contexte de thème à toute l'app */}
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
+    </AuthContextProvider>
   );
 }
 
-// Exportez l'application enveloppée avec appWithTranslation
-export default appWithTranslation(MyApp); // <-- MODIFIEZ CETTE LIGNE
+export default appWithTranslation(MyApp);
