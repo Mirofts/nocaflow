2efece1a945cc977748a2b268955742c51f95d21
Mirofts <mirofts@gmail.com>
2025-07-02 16:10:13 +0000

    Fix: Corrected i18next initialization by passing pageProps to appWithTranslation in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,13 @@
 import '@/styles/globals.css';
 import { appWithTranslation } from 'next-i18next';
-import { AuthContextProvider } from '../context/AuthContext'; // <-- Importez AuthContextProvider
-import { ThemeProvider } from '../context/ThemeContext';     // <-- Importez ThemeProvider
+import { AuthContextProvider } from '../context/AuthContext';
+import { ThemeProvider } from '../context/ThemeContext';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) { // pageProps est reçu ici
   console.log("MyApp simple test rendering"); // Debug log
   return (
-    // Enveloppez l'application avec vos Context Providers
-    <AuthContextProvider> {/* Fournit le contexte d'authentification à toute l'app */}
-      <ThemeProvider> {/* Fournit le contexte de thème à toute l'app */}
+    <AuthContextProvider>
+      <ThemeProvider>
         <main>
           <Component {...pageProps} />
         </main>
@@ -17,4 +16,6 @@ function MyApp({ Component, pageProps }) {
   );
 }
 
-export default appWithTranslation(MyApp);
+// MODIFICATION CRUCIALE : Passez pageProps à appWithTranslation
+// C'est ainsi que next-i18next reçoit les traductions de getStaticProps/getServerSideProps
+export default appWithTranslation(MyApp, { pageProps }); // <-- MODIFIEZ CETTE LIGNE
