4b2d59bcc8dde0491cd7c2d77fd7411515ed6dd9
Mirofts <mirofts@gmail.com>
2025-07-03 06:21:15 +0000

    ✅ test navbar 

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,16 +2,23 @@ import '@/styles/globals.css';
 import { appWithTranslation } from 'next-i18next';
 import { AuthContextProvider } from '../context/AuthContext';
 import { ThemeProvider } from '../context/ThemeContext';
+
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer'; // Facultatif, tu peux le retirer si tu n’en veux pas
 
 function MyApp({ Component, pageProps }) {
-  console.log("MyApp simple test rendering");
   return (
     <AuthContextProvider>
       <ThemeProvider>
-        <main>
-          <Component {...pageProps} />
-        </main>
+        <div className="flex flex-col min-h-screen bg-white text-black">
+          <Navbar />
+
+          <main className="flex-1 px-4 py-8 sm:px-6 lg:px-8">
+            <Component {...pageProps} />
+          </main>
+
+          <Footer />
+        </div>
       </ThemeProvider>
     </AuthContextProvider>
   );
