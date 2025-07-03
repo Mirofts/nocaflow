622105a14aeaba5b4399b2d1e7754b98ed1e7792
Mirofts <mirofts@gmail.com>
2025-07-03 05:37:23 +0000

    🛠 Fix: Vercel build import issue with next-i18next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 // next.config.mjs
 
-import { i18n } from './next-i18next.config'; // Change require to import
+import { i18n } from './next-i18next.config.js';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
